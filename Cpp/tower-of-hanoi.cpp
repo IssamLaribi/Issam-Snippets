99e110d6b8ab538face63a8340a9d1a1c594773d
IssamLRB <laribiissam8@gmail.com>
2025-09-01 23:48:05 +0100

    Update tower-of-hanoi.cpp

--- Cpp/tower-of-hanoi.cpp
+++ Cpp/tower-of-hanoi.cpp
@@ -2,12 +2,19 @@
 using namespace std;
 
 void towerOfHanoi(int n, int start, int dest) { 
-    if (n == 1)
-    {
+    if (n == 1) {
         cout << start << " to " << dest << "\n";
         return;
     }
     towerOfHanoi(n - 1, start, 6 - (start + dest));
     cout << start << " to " << dest << "\n";
     towerOfHanoi(n - 1, 6 - (start + dest), dest);
+}
+
+void towerOfHanoi(int n, int start, int end, int dest) {
+    if (n == 1) {
+        cout << start << " to " << dest << "\n";
+        return;
+    }
+    
 }
