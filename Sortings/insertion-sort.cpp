e1f149873bec66098d07a6421b54b7d7e3e3c430
IssamLRB <laribiissam8@gmail.com>
2025-09-02 02:24:23 +0100

    Update insertion-sort.cpp

--- Sortings/insertion-sort.cpp
+++ Sortings/insertion-sort.cpp
@@ -1,12 +1,11 @@
 void insertionSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        int key = arr[i + 1];
-        if (arr[i] > key) {
-            for (int j = i; j <= 0; j--) {
-                
-            }
-        } else {
-            
+    for (int i = 1; i < n - 1; i++) {
+        int key = arr[i];
+        int j = i - 1; 
+        while (j >= 0 && key < arr[j]) {
+            arr[j + 1] = arr[j];
+            j--;
         }
+        arr[j + 1] = key;
     }
 }
