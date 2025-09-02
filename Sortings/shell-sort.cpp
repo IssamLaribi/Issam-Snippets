f062305939536d5fe430bdd0f51d979309168854
IssamLRB <laribiissam8@gmail.com>
2025-09-02 03:04:01 +0100

    Update shell-sort.cpp

--- Sortings/shell-sort.cpp
+++ Sortings/shell-sort.cpp
@@ -1,1 +1,13 @@
-
+// Time complexity depends on gap sequence: typically O(n^1.5) to O(n^2), Space: O(1).
+void shellSort(int arr[], int n) {
+    for (int gap = n / 2; gap > 0; gap /= 2) {
+        for (int i = gap; i < n -  1; i++) {
+            int temp = arr[i];
+            int j;
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {  
+                arr[j] = arr[j - gap];           
+            }
+            arr[j] = temp;  
+        }
+    }
+}
