7ce9b0f6a0bd34f97a913d85f14d54fcc0e44d69
IssamLRB <laribiissam8@gmail.com>
2025-09-02 02:04:15 +0100

    Update insertion-sort.cpp

--- Sortings/insertion-sort.cpp
+++ Sortings/insertion-sort.cpp
@@ -1,1 +1,12 @@
-
+void insertionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int key = arr[i + 1];
+        if (arr[i] > key) {
+            for (int j = i; j <= 0; j--) {
+                
+            }
+        } else {
+            
+        }
+    }
+}
