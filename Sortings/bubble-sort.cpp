0fa6b77588f3ea08eaa096e83f9ffad9c01a9026
IssamLRB <laribiissam8@gmail.com>
2025-09-04 19:40:56 +0100

    Update bubble-sort.cpp

--- Sortings/bubble-sort.cpp
+++ Sortings/bubble-sort.cpp
@@ -1,7 +1,7 @@
 // Best: O(n), Average: O(n^2), Worst: O(n^2), Space: O(1).
 void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - 1; j++) {
+        for (int j = 0; j < n - i- 1; j++) {
             if (arr[j] > arr[j + 1]) swap(arr[j], arr[j + 1]); 
         }
     }
